7f1e8e63ca0b721bb5a4b96aa2121a88dbd9e1d5
Chealyfey Vutha <99248441+lyfeyvutha@users.noreply.github.com>
2023-04-14 22:58:17 +0700

    Create character_level_rnn.py

---
+++ character_level_rnn.py
@@ -0,0 +1,226 @@
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+# open text file and read in data as `text`
+with open('anna.txt', 'r') as f:
+    text = f.read()
+
+chars = tuple(set(text))
+int2char = dict(enumerate(chars))
+char2int = {ch: ii for ii, ch in int2char.items()}
+
+def one_hot_encode(arr, n_labels):
+    
+    # Initialize the encoded array
+    one_hot = np.zeros((arr.size, n_labels), dtype=np.float32)
+    
+    # Fill the appropriate elements with ones
+    one_hot[np.arange(one_hot.shape[0]), arr.flatten()] = 1.
+    
+    # Finally reshape it to get back to the original array
+    one_hot = one_hot.reshape((*arr.shape, n_labels))
+    
+    return one_hot
+
+def get_batches(arr, batch_size, seq_length):
+    '''Create a generator that returns batches of size
+       batch_size x seq_length from arr.
+       
+       Arguments
+       ---------
+       arr: Array you want to make batches from
+       batch_size: Batch size, the number of sequences per batch
+       seq_length: Number of encoded chars in a sequence
+    '''
+    
+    ## TODO: Get the number of batches we can make
+    total_batch_size = batch_size * seq_length
+    n_batches = len(arr) // total_batch_size
+    
+    ## TODO: Keep only enough characters to make full batches
+    arr = arr[:n_batches * total_batch_size]
+    
+    ## TODO: Reshape into batch_size rows
+    arr = arr.reshape((batch_size, -1))
+    
+    ## TODO: Iterate over the batches using a window of size seq_length
+    for n in range(0, arr.shape[1], seq_length):
+        # The features
+        x = arr[:, n:n+seq_length]
+        # The targets, shifted by one
+        y = np.zeros_like(x)
+        try:
+            y[:, :-1], y[:, -1] = x[:, 1:], arr[:, n+seq_length]
+        except IndexError:
+            y[:, :-1], y[:, -1] = x[:, 1:], arr[:, 0]
+        yield x, y
+
+# encode the text
+encoded = np.array([char2int[ch] for ch in text])
+
+def train(net, data, epochs=10, batch_size=10, seq_length=50, lr=0.001, clip=5, val_frac=0.1, print_every=10):
+    ''' Training a network 
+    
+        Arguments
+        ---------
+        
+        net: CharRNN network
+        data: text data to train the network
+        epochs: Number of epochs to train
+        batch_size: Number of mini-sequences per mini-batch, aka batch size
+        seq_length: Number of character steps per mini-batch
+        lr: learning rate
+        clip: gradient clipping
+        val_frac: Fraction of data to hold out for validation
+        print_every: Number of steps for printing training and validation loss
+    
+    '''
+    net.train()
+    
+    opt = torch.optim.Adam(net.parameters(), lr=lr)
+    criterion = nn.CrossEntropyLoss()
+    
+    # create training and validation data
+    val_idx = int(len(data)*(1-val_frac))
+    data, val_data = data[:val_idx], data[val_idx:]
+    
+    if(train_on_gpu):
+        net.cuda()
+    
+    counter = 0
+    n_chars = len(net.chars)
+    for e in range(epochs):
+        # initialize hidden state
+        h = net.init_hidden(batch_size)
+        
+        for x, y in get_batches(data, batch_size, seq_length):
+            counter += 1
+            
+            # One-hot encode our data and make them Torch tensors
+            x = one_hot_encode(x, n_chars)
+            inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
+            
+            if(train_on_gpu):
+                inputs, targets = inputs.cuda(), targets.cuda()
+
+            # Creating new variables for the hidden state, otherwise
+            # we'd backprop through the entire training history
+            h = tuple([each.data for each in h])
+
+            # zero accumulated gradients
+            net.zero_grad()
+            
+            # get the output from the model
+            output, h = net(inputs, h)
+            
+            # calculate the loss and perform backprop
+            loss = criterion(output, targets.view(batch_size*seq_length).long())
+            loss.backward()
+            # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+            nn.utils.clip_grad_norm_(net.parameters(), clip)
+            opt.step()
+            
+            # loss stats
+            if counter % print_every == 0:
+                # Get validation loss
+                val_h = net.init_hidden(batch_size)
+                val_losses = []
+                net.eval()
+                for x, y in get_batches(val_data, batch_size, seq_length):
+                    # One-hot encode our data and make them Torch tensors
+                    x = one_hot_encode(x, n_chars)
+                    x, y = torch.from_numpy(x), torch.from_numpy(y)
+                    
+                    # Creating new variables for the hidden state, otherwise
+                    # we'd backprop through the entire training history
+                    val_h = tuple([each.data for each in val_h])
+                    
+                    inputs, targets = x, y
+                    if(train_on_gpu):
+                        inputs, targets = inputs.cuda(), targets.cuda()
+
+                    output, val_h = net(inputs, val_h)
+                    val_loss = criterion(output, targets.view(batch_size*seq_length).long())
+                
+                    val_losses.append(val_loss.item())
+                
+                net.train() # reset to train mode after iterationg through validation data
+                
+                print("Epoch: {}/{}...".format(e+1, epochs),
+                      "Step: {}...".format(counter),
+                      "Loss: {:.4f}...".format(loss.item()),
+                      "Val Loss: {:.4f}".format(np.mean(val_losses)))
+train_on_gpu = torch.cuda.is_available()
+if(train_on_gpu):
+    print('Training on GPU!')
+else: 
+    print('No GPU available, training on CPU; consider making n_epochs very small.')
+
+class CharRNN(nn.Module):
+    
+    def __init__(self, tokens, n_hidden=256, n_layers=2,
+                               drop_prob=0.5, lr=0.001):
+        super().__init__()
+        self.drop_prob = drop_prob
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.lr = lr
+        
+        # creating character dictionaries
+        self.chars = tokens
+        self.int2char = dict(enumerate(self.chars))
+        self.char2int = {ch: ii for ii, ch in self.int2char.items()}
+        
+        ## TODO: define the layers of the model
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(n_hidden, len(self.chars))
+    
+    def forward(self, x, hidden):
+        ''' Forward pass through the network. 
+            These inputs are x, and the hidden/cell state `hidden`. '''
+                
+        ## TODO: Get the outputs and the new hidden state from the lstm
+        self.lstm = nn.LSTM(len(self.chars), n_hidden, n_layers, 
+                            dropout=drop_prob, batch_first=True)
+        
+        r_output, hidden = self.lstm(x, hidden)
+        out = self.dropout(r_output)
+        out = out.contiguous().view(-1, self.n_hidden)
+        out = self.fc(out)
+        # return the final output and the hidden state
+        return out, hidden
+    
+    
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x n_hidden,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+        
+        if (train_on_gpu):
+            hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(),
+                  weight.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_(),
+                      weight.new(self.n_layers, batch_size, self.n_hidden).zero_())
+        
+        return hidden
+
+
+## TODO: set your model hyperparameters
+# define and print the net
+n_hidden= 128
+n_layers= 4
+
+net = CharRNN(chars, n_hidden, n_layers)
+
+drop_prob = 0.5
+batch_size = 16
+seq_length = 40
+n_epochs = 1 # start small if you are just testing initial behavior
+
+
+# train the model
+train(net, encoded, epochs=n_epochs, batch_size=batch_size, seq_length=seq_length, lr=0.001, print_every=10)
